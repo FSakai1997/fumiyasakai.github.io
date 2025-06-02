eb0fbb2b618ae643a731fc34c7159e8609a4f67f
fsakai <johnsandars64@gmail.com>
2025-06-02 15:26:10 +0900

    update

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 // ニュースを動的に追加したい場合、JavaScriptで内容を更新できます。
 const recentNews = [
-    { date: "2025年2月25日", content: "共著論文がPRLで出版されました" },
+    { date: "2025年5月30日", content:"JpGU2025で発表しました。"}
+    { date: "2025年2月25日", content: "共著論文がPRLで出版されました。" },
     { date: "2024年12月09日", content: "AGU24に参加しました。" },
     { date: "2024年9月3日", content: "IPGPに訪問しています。" },
-    { date: "2024年7月12日", content: "JpGU2024学生発表優秀賞を受賞しました。" },
-    { date: "2024年5月20日", content:"共著論文がarXivにあがりました。"}
+    { date: "2024年7月12日", content: "JpGU2024学生発表優秀賞を受賞しました。" }
 ];
 
 const newsList = document.getElementById("recent-news");
