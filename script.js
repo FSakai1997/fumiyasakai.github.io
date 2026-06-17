7def4f22b636a5fb59fe098cf67cba56975af82b
fsakai <johnsandars64@gmail.com>
2026-06-17 20:32:40 +0900

    update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // ニュースを動的に追加したい場合、JavaScriptで内容を更新できます。
 const recentNews = [
+    { date: "2026年6月17日", content:"SPring-8大学院生課題優秀研究賞を受賞しました。"},
     { date: "2026年5月27日", content:"JpGU2026で口頭発表を行いました"},
     { date: "2026年4月13日", content:"共著論文がGCAで出版されました。"},
     { date: "2026年3月24日", content:"博士課程を修了しました。"},
